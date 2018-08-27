603f1153470ee5162f86ccc919d01dc24ca717a3
Marco Origlia <m.origlia@outlook.it>
2018-08-27 22:47:46 +0200

    Written AjaxClient, small ajax manager for the client side, NOT TESTED

---
+++ js/ajax/AjaxClient.js
@@ -0,0 +1,48 @@
+// Essentially copyed from Lab9 ------------------------------------------------
+function AjaxClient(){
+    this.getClient = function () {
+        var xmlHttp = null;
+        try{
+            xmlHttp = new XMLHttpRequest();
+        } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+					xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch (e) {
+					xmlHttp = null;
+				}
+            }
+        }
+    }
+
+    this.request = function(method, url, data, responseHandler){
+        var client = AjaxClient.getClient();
+        if(!c){
+            window.alert("AJAX required, but not supported by your browser. You can no longer use this site.");
+            return false;
+        }
+
+        client.open(method, url, true);
+        if(data && method == "POST" ){
+            client.setRequestHeader('Content-Type', 'application/json');
+            data = JSON.stringify(data);
+        } // GET + data not handled
+        client.onreadystatechange = function(){
+            if(client.readyState == 4){
+                var response = JSON.parse(client.responseText);
+                responseHandler(response);
+            }
+        }
+        client.send(data);
+    }
+
+    this.get = function (url, responseHandler) {
+        return this.request("GET", url, null, responseHandler);
+    }
+
+    this.post = function (url, data, responseHandler) {
+        return this.request("POST", url, data, responseHandler);
+    }
+}
